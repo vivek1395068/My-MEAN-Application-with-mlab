35f6bb9d70b127a2f100dc0199beca243b97cf3c
vivek1395068 <vivek1395068@gmail.com>
2018-03-09 17:02:34 +0530

    Add files via upload

---
+++ app/service/task.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {Http,Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Json } from '@angular/core/src/facade/lang';
+
+@Injectable()
+export class TaskService{
+    constructor(private http:Http){
+        console.log('Task Service Initiated...');
+    }
+
+    getTasks(){
+        return this.http.get('http://localhost:3008/api/tasks')
+            .map(res=> res.json())
+    }
+
+    addTask(newTask){
+        var headers= new Headers();
+        headers.append('Content-Type','application/json');
+        return this.http.post('http://localhost:3008/api/task',JSON.stringify(newTask),{headers:headers}).map
+        (res=>res.json());
+    }
+    
+    deleteTask(id){
+        return this.http.delete('/api/task/'+id)
+        .map(res=>res.json())
+    }
+
+    updateStatus(task){
+        var headers=new Headers();
+        headers.append('Content-Type','application/json');
+        return this.http.put('/api/task/'+task._id,JSON.stringify(task),{headers:headers})
+        .map(res=>res.json());
+    }
+}
